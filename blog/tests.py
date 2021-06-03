6df96a62581f4d6de7c81537c4e8a7beffea12a7
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-03 16:07:05 +0800

    initial commit

---
+++ blog/tests.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+
+        self.post = Post.objects.create(
+            title='great title',
+            body='good body',
+            author=self.user
+        )
+
+    def test_string_representation(self):
+        post = Post(title='simple title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'good body')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'great title')
+        self.assertTemplateUsed(response, 'post_detail.html')
